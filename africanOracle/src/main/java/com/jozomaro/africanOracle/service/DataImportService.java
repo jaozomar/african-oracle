f3973fdb9e15037f949c9c5aa9da8b52c5f0d994
joshua <ozomaroj@gmail.com>
2026-05-03 14:24:23 -0700

    trying to read file

--- africanOracle/src/main/java/com/jozomaro/africanOracle/service/DataImportService.java
+++ africanOracle/src/main/java/com/jozomaro/africanOracle/service/DataImportService.java
@@ -1,1 +1,40 @@
 package com.jozomaro.africanOracle.service;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.springframework.stereotype.Service;
+
+import com.jozomaro.africanOracle.repository.ArtifactRepository;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
+@Service // "Let's know spring this is a specialist class"
+@RequiredArgsConstructor // "Lombok, give me a constructor for the Repository"
+public class DataImportService {
+    private final ArtifactRepository repository;
+    
+    @PostConstruct // "Spring, run this the moment you finish setting me up"
+    public void importData() {
+        // get nigerian artifacts csv file
+        InputStream nigerianArtifacts = getClass().getResourceAsStream("resources/nigeriaArtifacts.csv");
+        
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(nigerianArtifacts))) {
+            // can handle exception if reader fails to open
+            String i; // i for input. Will hold input read from csv file
+
+            // print out all lines of the csv file
+            while( (i = reader.readLine()) != null) {
+                System.out.println("Found: " + i);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Data has been read!");
+    }
+}
